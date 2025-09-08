64c6152a3314339fa966a643517dc5e1e19b3615
Malika-ui969 <begimqulovamalika616@gmail.com>
2025-09-08 16:28:53 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import os
 
 # 🔑 Tokenlar
 TOKEN = "8017914106:AAH37SYFd_hKz1BfzAct5dScXw_INGsUaL8"
-HF_TOKEN = "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"
+HF_TOKEN = "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"
 CHANNEL_ID = "-1002752698442"
 BOT_USERNAME = "ebblender3D_bot"
 
